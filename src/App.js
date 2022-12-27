79548d7b9e4e18bbdbf9919ab7fbee8c83249740
Rosyidimalang <ahmadrosyidi432@gmail.com>
2022-12-27 10:59:33 +0700

    add data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,38 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+const menus = ["makanan", "minuman", "lainnya"];
+const product = {
+  makanan: [
+    { nama: "sate Ayam", harga: 16000, gambar: "sate-ayam.jpg" },
+    {
+      nama: "Nasi Goreng Telur",
+      harga: 14000,
+      gambar: "nasi-goreng-telor.jpg",
+    },
+    { nama: "Nasi Rames", harga: 12000, gambar: "nasi-rames.jpg" },
+    {
+      nama: "Lontong Opor Ayam",
+      harga: 18000,
+      gambar: "lontong-opor-ayam.jpg",
+    },
+    { nama: "Mie Goreng", harga: 13000, gambar: "mie-goreng.jpg" },
+    { nama: "Bakso", harga: 10000, gambar: "bakso.jpg" },
+    { nama: "Mie Ayam Bakso", harga: 14000, gambar: "mie-ayam-bakso.jpg" },
+  ],
+  minuman: [
+    { nama: "Coffe Late", harga: 15000, gambar: "coffe-late.jpg" },
+    { nama: "Es Jeruk", harga: 7000, gambar: "es-jeruk.jpg" },
+    { nama: "Es Teh", harga: 5000, gambar: "es-teh.jpg" },
+    { nama: "Teh Hangat", harga: 3000, gambar: "teh-hangat.jpg" },
+  ],
+  lainnya: [
+    { nama: "Pangsit 6 pcs", harga: 5000, gambar: "pangsit.jpg" },
+    { nama: "Kentang Goreng", harga: 5000, gambar: "kentang-goreng.jpg" },
+    { nama: "Cheese Burger", harga: 15000, gambar: "cheese-burger.jpg" },
+  ],
+};
+
 function App() {
   return <div></div>;
 }
