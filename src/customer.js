9c2487dfd043530284493725f1d08133f2e3d274
Rosyidimalang <ahmadrosyidi432@gmail.com>
2023-01-04 10:02:52 +0700

    get data from localstorage

--- src/customer.js
+++ src/customer.js
@@ -1,8 +1,30 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-export default function customer() {
-  return <div>customer</div>;
+export default function Customer() {
+  let value = JSON.parse(localStorage.getItem("customers"));
+  let prettyData = JSON.stringify(value, null, 2);
+
+  return (
+    <div className="border rounded-xl bg-slate-300">
+      <pre className="font-mono whitespace-pre-wrap">{prettyData}</pre>;
+    </div>
+  );
+
+  // let customers = JSON.parse(value)[0];
+
+  // console.log("tes", customers);
+
+  // return (
+  //   <div>
+  //     {customers.map((customer) => (
+  //       <div key={customer.id}>
+  //         <p>Nama: {customer.nama}</p>
+  //         <p>Alamat: {customer.alamat}</p>
+  //       </div>
+  //     ))}
+  //   </div>
+  // );
 }
 
 // ambil data dari localStorage
